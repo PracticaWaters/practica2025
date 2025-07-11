3a2bfbb2a529dcf473705814b8c72203bbb95f63
laurazota <zota.laura.stefania@gmail.com>
2025-07-11 11:25:22 +0300

    Created Support Page

--- CinemaApp/src/app/app-module.ts
+++ CinemaApp/src/app/app-module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Support } from './support/support';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Support
   ],
   imports: [
     BrowserModule,
---
+++ CinemaApp/src/app/support/support.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-support',
+  standalone: false,
+  templateUrl: './support.html',
+  styleUrl: './support.css'
+})
+export class Support {
+
+}
