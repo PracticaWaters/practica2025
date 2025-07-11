e5a1caf552ff453351446179cf26e8481c23af1f
deniiru <rujoiudenisa2004@gmail.com>
2025-07-11 14:27:41 +0300

    [add] user controll

---
+++ CinemaAPI/CinemaAPI/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using CinemaAPI.DataManagement;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaAPI.Controllers
+{
+    [ApiController]
+    [Route("api/cinema/user")]
+    public class UserController : Controller
+    {
+        private readonly UserDataOps userDataOps;
+       
+        public UserController()
+        {
+            userDataOps = new UserDataOps();
+        }
+
+    }
+}
---
+++ CinemaAPI/CinemaAPI/DataManagement/UserDataOps.cs
@@ -0,0 +1,20 @@
+﻿namespace CinemaAPI.DataManagement
+{
+    public class UserDataOps
+    {
+        private CinemaDbContext dbContext { get; set; }
+
+        public UserDataOps()
+        {
+            dbContext = new CinemaDbContext();
+        }
+
+        // add
+
+        // get 
+
+        // update
+
+        // delete
+    }
+}
