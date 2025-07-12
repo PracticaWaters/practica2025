dc955e3f9a1afdc05171fb130306e174b183ac40
deniiru <rujoiudenisa2004@gmail.com>
2025-07-12 11:27:43 +0300

    feat(user): complete user controller

--- CinemaAPI/CinemaAPI/Controllers/UserController.cs
+++ CinemaAPI/CinemaAPI/Controllers/UserController.cs
@@ -1,10 +1,11 @@
 ﻿using CinemaAPI.DataManagement;
+using CinemaAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CinemaAPI.Controllers
 {
     [ApiController]
-    [Route("api/cinema/user")]
+    [Route("api/cinema/user/")]
     public class UserController : Controller
     {
         private readonly UserDataOps userDataOps;
@@ -13,6 +14,118 @@ namespace CinemaAPI.Controllers
         {
             userDataOps = new UserDataOps();
         }
+
+
+        [HttpPost]
+        public ActionResult AddUser(User user)
+        {
+            try
+            {
+                userDataOps.AddUser(user);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+           
+        }
+
+        [HttpPut]
+        public ActionResult UpdateUser(User user)
+        {
+            try
+            {
+                userDataOps.UpdateUser(user);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        [HttpDelete]
+        public ActionResult DeleteUser(User user)
+        {
+            try
+            {
+                userDataOps.DeleteUser(user);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        [HttpGet]
+        public ActionResult<User[]> GetUsers()
+        {
+            try
+            {
+                var user = userDataOps.GetUsers();
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpGet("{id}")]
+        public ActionResult<User?> GetUserById([FromRoute] int id)
+        {
+            try
+            {
+                var user = userDataOps.GetUserById(id);
+                if (user == null)
+                    return NotFound($"User with ID {id} was not found.");
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("email")]
+        public ActionResult<User?> GetUserByEmail([FromQuery] string email)
+        {
+            try
+            {
+                var user = userDataOps.GetUserByEmail(email);
+                if (user == null)
+                    return NotFound($"User with email {email} was not found.");
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpPost("personal-information")]
+        public ActionResult<User?> GetUserByEmailAndPassword([FromQuery] string email, [FromQuery] string password)
+        {
+            try
+            {
+                var user = userDataOps.GetUserByEmailAndPassword(email, password);
+                if (user == null)
+                    return NotFound($"User was not found.");
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
 
     }
 }
--- CinemaAPI/CinemaAPI/DataManagement/UserDataOps.cs
+++ CinemaAPI/CinemaAPI/DataManagement/UserDataOps.cs
@@ -1,4 +1,7 @@
-﻿namespace CinemaAPI.DataManagement
+﻿using CinemaAPI.Models;
+using System.Linq.Expressions;
+
+namespace CinemaAPI.DataManagement
 {
     public class UserDataOps
     {
@@ -10,11 +13,79 @@
         }
 
         // add
+        public void AddUser(User user)
+        {
+            if (GetUserByEmail(user.Email) != null)
+                throw new Exception($"User with email '{user.Email}' already exists.");
+           
+            try
+            {
+                dbContext.Add(user);
+                dbContext.SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to add a new user");
+            }
+        }
 
         // get 
 
+        public User[] GetUsers ()
+        {
+            return dbContext.users.Where(u => u.IsDeleted == false).ToArray();
+        }
+
+        public User? GetUserById (int id)
+        {
+            return dbContext.users.Where(u => u.IsDeleted==false && u.Id == id).FirstOrDefault();
+        }
+
+
+        public User? GetUserByEmail(string email)
+        {
+            return dbContext.users
+                .Where(u => u.IsDeleted == false && u.Email == email)
+                .FirstOrDefault();
+        }
+        public User? GetUserByEmailAndPassword (string email, string password)
+        {
+            return dbContext.users
+                .Where(u => u.IsDeleted == false && u.Email == email && u.Password == password)
+                .FirstOrDefault();
+        }
+
         // update
+        public void UpdateUser(User user)
+        {
+            try
+            {
+                user.ModifiedAt = DateTime.Now;
+                dbContext.Update(user);
+                dbContext.SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to update the user");
+            }
+        }
 
         // delete
+        public void DeleteUser(User user)
+        {
+            try
+            {
+                user.IsDeleted = true;
+                dbContext.Update(user);
+                dbContext.SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to delete the user");
+            }
+        }
     }
 }
