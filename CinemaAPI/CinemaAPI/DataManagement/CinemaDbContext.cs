03c78f57c7334a2e2fe4d6270b51e1f37bc61e27
Prodan Cosmin Daniel <pro.cosmin2004@gmail.com>
2025-07-14 12:33:19 +0300

    Update CinemaDbContext.cs

--- CinemaAPI/CinemaAPI/DataManagement/CinemaDbContext.cs
+++ CinemaAPI/CinemaAPI/DataManagement/CinemaDbContext.cs
@@ -1,18 +1,25 @@
+
+﻿using Microsoft.EntityFrameworkCore;
+using CinemaAPI.Models;
+
 ﻿using CinemaAPI.Models;
 using Microsoft.EntityFrameworkCore;
-using CinemaAPI.Models;
+
 
 namespace CinemaAPI.DataManagement
 {
     public class CinemaDbContext : DbContext
     {
+
+        public DbSet<SuportTicket> SuportTickets { get; set; }
+
         public DbSet<User> users { get; set; }
 
         public DbSet<Cinema> cinemas { get; set; }
 
         public DbSet<Film> films { get; set; }
         
-        public DbSet<SuportTicket> SuportTickets { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Cinema;Trusted_Connection=True;MultipleActiveResultSets=true");
