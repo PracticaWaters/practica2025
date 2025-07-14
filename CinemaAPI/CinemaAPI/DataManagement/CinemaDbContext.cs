9f0c0040ca9410070eaaf93f4cba6d4ce14dc0bf
Andrei Moroianu <madalin.moroianu@student.unitbv.ro>
2025-07-14 10:41:34 +0300

    Gr1 user data model without hashing (#16)
    * [add] user model
    
    * [add] user in Database
    
    * [add] user controll
    
    * feat(user): complete user controller
    
    ---------
    
    Co-authored-by: deniiru <rujoiudenisa2004@gmail.com>
    Co-authored-by: Eduard Mihaiu <132848172+eduskm@users.noreply.github.com>

--- CinemaAPI/CinemaAPI/DataManagement/CinemaDbContext.cs
+++ CinemaAPI/CinemaAPI/DataManagement/CinemaDbContext.cs
@@ -5,7 +5,9 @@ namespace CinemaAPI.DataManagement
 {
     public class CinemaDbContext : DbContext
     {
+        public DbSet<User> users { get; set; }
         public DbSet<Film> films { get; set; }
+        
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Cinema;Trusted_Connection=True;MultipleActiveResultSets=true");
