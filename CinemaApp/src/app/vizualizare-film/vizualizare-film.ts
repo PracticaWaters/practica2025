51e9d666f57c0ededa9eb3822f8cf08af69db863
Raul Gavriluta <raul.gavriluta7@gmail.com>
2025-07-15 11:07:01 +0300

    vizualizare film + navigatie

--- CinemaApp/src/app/vizualizare-film/vizualizare-film.ts
+++ CinemaApp/src/app/vizualizare-film/vizualizare-film.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-vizualizare-film',
   standalone: false,
   templateUrl: './vizualizare-film.html',
-  styleUrl: './vizualizare-film.css'
+  styleUrl: './vizualizare-film.css',
+  encapsulation: ViewEncapsulation.None,
+
 })
 export class VizualizareFilm {
 
