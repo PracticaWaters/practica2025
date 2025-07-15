04eb419d461be8367688b33aae508a3b21486895
Ana-MariaC <ana_maria.ciobanu@student.unitbv.ro>
2025-07-15 12:04:01 +0300

    Pagina Program Cinema

--- CinemaApp/src/app/app-module.ts
+++ CinemaApp/src/app/app-module.ts
@@ -2,18 +2,34 @@ import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {MatMenuModule} from '@angular/material/menu'
 import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { VizualizareFilm } from './vizualizare-film/vizualizare-film';
+import { ProgramCinema } from './program-cinema/program-cinema';
+
 
 @NgModule({
   declarations: [
-    App
+    App,
+    VizualizareFilm,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatInputModule,
-    MatMenuModule
+    MatMenuModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule,
+    MatSelectModule,
+    FormsModule,
+    ProgramCinema
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
