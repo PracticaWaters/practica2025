2402d4ec2d36ecec2f91c6bfcc3e3361c49d9615
Marco <tg6727955@gmail.com>
2025-07-16 11:33:13 +0300

    added timeslot model

---
+++ CinemaApp/src/app/app-logic/timeslot.ts
@@ -0,0 +1,9 @@
+import { ScreeningRoomData } from "./screening-room-data";
+
+export class timeslot{
+    id!: number;
+    startTime!: timeslot;
+    endTime!: timeslot;
+    screeningRoom!: ScreeningRoomData;
+    //movie!: 
+}
