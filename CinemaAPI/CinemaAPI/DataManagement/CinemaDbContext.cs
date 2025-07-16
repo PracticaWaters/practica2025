4b9fa20ee49c3ed36e4c25d376abf5be71e765cb
Radu <raduandreicapatina@yahoo.com>
2025-07-16 09:08:59 +0200

    Added Wishlists (#29)

--- CinemaAPI/CinemaAPI/DataManagement/CinemaDbContext.cs
+++ CinemaAPI/CinemaAPI/DataManagement/CinemaDbContext.cs
@@ -7,12 +7,14 @@ namespace CinemaAPI.DataManagement
     public class CinemaDbContext : DbContext
     {
         public DbSet<User> users { get; set; }
-
         public DbSet<Cinema> cinemas { get; set; }
-
         public DbSet<Film> films { get; set; }
         public DbSet<Review> reviews { get; set; }
+        public DbSet<Wishlist> wishlists { get; set; }
+        public DbSet<Format> formats { get; set; }
+        public DbSet<Actor> actors { get; set; }
         public DbSet<Rezervare> rezervari { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Cinema;Trusted_Connection=True;MultipleActiveResultSets=true");
@@ -27,7 +29,19 @@ namespace CinemaAPI.DataManagement
                 .HasForeignKey("FilmId")
                 .IsRequired();
 
+            modelBuilder.Entity<User>()
+                .HasMany(w => w.Wishlists)
+                .WithOne(u => u.User);
+
             modelBuilder.Entity<Film>()
+                .HasMany(w => w.Wishlists)
+                .WithOne(f => f.Film);
+
+            modelBuilder.Entity<Film>()
+                .HasMany(f => f.FilmActors)
+                .WithMany(a => a.FilmActors)
+                .UsingEntity(j => j.ToTable("FilmActor"));
+
                 .HasMany(r => r.Rezervari)
                 .WithOne(g => g.Film)
                 .IsRequired();
