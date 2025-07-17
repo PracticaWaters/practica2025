e5072b17e4c9554edb4f76760fdc5803a367f0a6
Negoita Andrei <lilbabyfan04@gmail.com>
2025-07-17 11:26:37 +0300

    Gr4 cinema room (#56)
    * Design Mai Smecher + Variabila Row din Seat => string
    
    * added timeslot list
    
    * Fixed styles.css
    
    ---------
    
    Co-authored-by: Iofise <alexiosif.2005@gmail.com>
    Co-authored-by: Marco <tg6727955@gmail.com>

--- CinemaApp/src/app/app-module.ts
+++ CinemaApp/src/app/app-module.ts
@@ -63,6 +63,7 @@ import { SupportAdmin } from './Support/support-admin/support-admin';
 import { SupportForm } from './Support/support-form/support-form';
 import { Faq } from './Support/faq/faq';
 import { RouterModule } from '@angular/router';
+import { TimeslotList } from './timeslot-operations/timeslot-list/timeslot-list';
 
 
 @NgModule({
@@ -84,6 +85,7 @@ import { RouterModule } from '@angular/router';
     SupportAdmin,
     SupportForm,
     Faq,
+    TimeslotList,
   ],
   imports: [
     BrowserModule,
