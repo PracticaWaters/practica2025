cf7f55a7aad6420f1eacd68b2907c0e7a523da35
Rares <petruraresd@gmail.com>
2025-07-18 13:34:39 +0300

    rezolv eu setari

--- CinemaApp/src/app/user-dashboard/pages/setari/setari.ts
+++ CinemaApp/src/app/user-dashboard/pages/setari/setari.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
-
 
 @Component({
   selector: 'app-setari',
@@ -10,14 +9,25 @@ import { MatDialog } from '@angular/material/dialog';
   styleUrl: './setari.css'
 })
 export class Setari {
-  confirmAndSubmit() {}
-  onCancel() {}
   form: FormGroup;
 
-    constructor(private fb: FormBuilder, private dialog: MatDialog) {
+  constructor(private fb: FormBuilder, private dialog: MatDialog) {
     this.form = this.fb.group({
-      'new-pass': [''],
-      'current-pass': ['']
+      currentPassword: ['', Validators.required],
+      newPassword: ['', Validators.required]
     });
+  }
+
+  confirmAndSubmit() {
+    if (this.form.valid) {
+      const { currentPassword, newPassword } = this.form.value;
+      console.log('Parolă curentă:', currentPassword);
+      console.log('Parolă nouă:', newPassword);
+      // TODO: Trimite datele către server
+    }
+  }
+
+  onCancel() {
+    this.form.reset();
   }
 }
