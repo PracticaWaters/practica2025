1556732e04b5c8f5b887f6b2f8c745cabef0b425
eduskm <eduard.mihaiu@student.unitbv.ro>
2025-07-18 11:07:45 +0300

    schimbat culoare text mat si animatii butoane

--- CinemaApp/src/app/user-dashboard/user-dashboard.ts
+++ CinemaApp/src/app/user-dashboard/user-dashboard.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-user-dashboard',
   standalone: false,
   templateUrl: './user-dashboard.html',
-  styleUrl: './user-dashboard.css'
+  styleUrl: './user-dashboard.css',
 })
 export class UserDashboard {
 }
